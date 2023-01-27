d3512f3842b932f106b9466f3ea0cd0a35c38fc5
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-01-27 17:39:13 +0000

    First view

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Rango says 'Hey there, partner!'")
