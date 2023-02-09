95e778053c3fa3b30eab27dede3bc7239cd70fab
eilidh <56912943+SMC242@users.noreply.github.com>
2023-02-09 02:42:56 +0000

    Registered models for admin dashboard

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from rango.models import Category, Page
+
 # Register your models here.
+models = [
+    Category,
+    Page
+]
+
+for m in models:
+    admin.site.register(m)
