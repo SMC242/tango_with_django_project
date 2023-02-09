b9d41d373ca46597955014ec0a0621b20a9e35ed
eilidh <56912943+SMC242@users.noreply.github.com>
2023-02-09 05:29:34 +0000

    Category slugs

--- rango/admin.py
+++ rango/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
+from django.db.models import Model
 
 from rango.models import Category, Page
+
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url")
-    
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
 # Register your models here.
-models = [
-    Category,
-]
+models: list[Model | admin.ModelAdmin] = []
 
 for m in models:
     admin.site.register(m)
