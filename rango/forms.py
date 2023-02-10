45bb93a1e9d126cb62772cf653137d6dcfb15728
eilidh <56912943+SMC242@users.noreply.github.com>
2023-02-10 07:23:00 +0000

    Chapter 7

---
+++ rango/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from rango.models import Page, Category
+
+TITLE_LENGTH = 128
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=TITLE_LENGTH, help_text="Please enter the category name."
+    )
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ("name",)
+
+
+def valid_url(s: str) -> bool:
+    return s.startswith("http://") or s.startswith("https://")
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=TITLE_LENGTH, help_text="Please enter the title of the page."
+    )
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ("category",)
+
+    def clean(self) -> dict:
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get("url")
+
+        if url and not valid_url(url):
+            cleaned_data["url"] = f"http://{url}"
+        return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
+from django.urls import reverse
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -35,3 +37,47 @@ def show_category(request: HttpRequest, category_name_slug: str) -> HttpResponse
         context = {"category": None, "pages": None}
 
     return render(request, "rango/category.html", context=context)
+
+
+def add_category(request: HttpRequest) -> HttpResponse:
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect("/rango/")
+        else:
+            print(form.errors)
+
+    return render(request, "rango/add_category.html", {"form": form})
+
+
+def add_page(request: HttpRequest, category_name_slug: str) -> HttpResponse:
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        return redirect("/rango/")
+
+    form = PageForm()
+    if request.method == "POST":
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            page = form.save(commit=True)
+            print(f"Adding to category: {category}")
+            page.category = category
+            page.views = 0
+            page.save()
+            return redirect(
+                reverse(
+                    "rango:show_category",
+                    kwargs={"category_name_slug": category_name_slug},
+                )
+            )
+        else:
+            print(form.errors)
+
+    context = {"form": form, "category": category}
+    return render(request, "rango/add_page.html", context=context)
